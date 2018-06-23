78913999928505e6c755d97fd6b4ec6e136dcd7e
Charles <dailiang18bb@gmail.com>
2018-06-23 09:33:04 -0400

    ....

---
+++ UIKit Fundamentals/Lesson 3 The Delegate Pattern/step3.1-textFieldsApp/TextFields/RandomColorTextFieldDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  RandomColorTextFieldDelegate.swift
+//  TextFields
+//
+//  Created by charles on 6/21/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RandomColorTextFieldDelegate: NSObject,UITextFieldDelegate {
+    
+    
+}
+
+
